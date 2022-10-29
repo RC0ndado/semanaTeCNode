f942b39931a68a570d3511f8b75c437ea42ee7ca
RC0ndado <81990698+RC0ndado@users.noreply.github.com>
2022-10-28 20:40:46 -0500

    Ultimos cambios

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const fileUpload = require('express-fileupload');
 const modeloIARoutes = require('./routes/modeloIA')
 const ejemploDBRoutes = require('./routes/ejemploBD')
 const sequelize = require('./utils/database')
+const cors = require('cors')
 
 //middlewares
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname, 'public')));
